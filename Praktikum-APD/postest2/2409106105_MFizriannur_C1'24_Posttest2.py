321f3ca74af383054f2d55829a866a485f8ae822
fizri907 <fizri907@gmail.com>
2024-09-17 18:49:09 +0800

    2409106105_MFizriannur_C1'24_Posttest2.py

--- Praktikum-APD/postest2/2409106105_MFizriannur_C1'24_Posttest2.py
+++ Praktikum-APD/postest2/2409106105_MFizriannur_C1'24_Posttest2.py
@@ -1,20 +1,19 @@
-nama = input("Muhammad Fizriannur")
-nim  = input("2409106105")
+nama = input("Masukkan Nama: ")
+nim  = input("Masukkan Nim :")
 HargaGula = 21000
 
 DiskonGulaku = 0.07 
 DiskonManisKita = 0.11
 DiskonGunungMadu = 0.13
 
-DiskonGulaku = HargaGula * DiskonGulaku
-DiskonManisKita = HargaGula * DiskonManisKita
-DiskonGunungMadu = HargaGula * DiskonGunungMadu
-
 HSDgulaku = HargaGula - (HargaGula * DiskonGulaku)
 HSDmaniskita = HargaGula - (HargaGula * DiskonManisKita)
 HSDgunungmadu = HargaGula - (HargaGula * DiskonGunungMadu)
- 
-print=(f"{nama} dengan NIM {nim} ingin membeli gula seharga Rp {HargaGula}")
-print=(f"Jika dia membeli Gulaku ia harus membayar {HSDgulaku} Setelah mendapat diskon 7%")
-print=(f"Jika dia membeli Manis Kita ia harus membayar {HSDmaniskita} Setelah mendapat diskon 11%")
-print=(f"Jika dia membeli Gunung Madu ia harus membayar {HSDgunungmadu}setelah mendapat diskon 13%")
+
+output = (
+    f"{nama} dengan NIM {nim} ingin membeli gula seharga Rp {HargaGula}"
+    f"Jika dia membeli Gulaku ia harus membayar {HSDgulaku} Setelah mendapat diskon 7%"
+    f"Jika dia membeli Manis Kita ia harus membayar {HSDmaniskita} Setelah mendapat diskon 11%"
+    f"Jika dia membeli Gunung Madu ia harus membayar {HSDgunungmadu}setelah mendapat diskon 13%"
+        )
+print(output) 
